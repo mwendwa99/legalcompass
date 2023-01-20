648a99c282462a6361e82a23170f7c4f2251dac3
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2023-01-20 10:18:08 +0300

    upfate get direcitons page

--- src/pages/GetDirection.jsx
+++ src/pages/GetDirection.jsx
@@ -1,12 +1,25 @@
-import { Box } from "@mui/material";
+import { Box, Typography, Container } from "@mui/material";
 import { Stepper, BreadCrumbs } from "../components";
 
 const GetDirection = () => {
   return (
-    <div>
+    <Box>
       <BreadCrumbs />
-      <Stepper />
-    </div>
+      <Container maxWidth="lg">
+        <Typography
+          align="center"
+          sx={{ fontWeight: 300 }}
+          variant="h2"
+          component="h2"
+          gutterBottom
+        >
+          Here we will be able to assist you in navigating the corridors of
+          justice.
+          <br /> Just follow these simple steps!
+        </Typography>
+        <Stepper />
+      </Container>
+    </Box>
   );
 };
 
